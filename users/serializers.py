e993541c963c53e88d100b5516d5e79c1963e227
Vika <vika.davlyatshina@mail.ru>
2026-05-04 01:14:28 +0200

    Добавлен сериализатор для telegram

--- users/serializers.py
+++ users/serializers.py
@@ -47,3 +47,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ["id", "email", "first_name", "last_name", "avatar"]
         read_only_fields = ["id", "email"]
+
+class TelegramLinkSerializer(serializers.Serializer):
+    """ Сериализатор для добавления Telegram ID """
+    telegram_chat_id = serializers.IntegerField()
