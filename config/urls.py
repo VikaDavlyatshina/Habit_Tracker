a8cbf863aa7e759984c7f4597a5569fac5a083f8
Vika <vika.davlyatshina@mail.ru>
2026-05-03 18:26:29 +0200

    Маршруты документации

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/habits/", include("habits.urls", namespace="habits")),
+    path("api/", include("habits.urls", namespace="habits")),
     path("api/users/", include("users.urls", namespace="users")),
+
+    # Документация API
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
+    path(
+        "api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
