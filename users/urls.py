d51eb9dbd1cc6012f9adfc1b78872d7fa7e243b5
Vika <vika.davlyatshina@mail.ru>
2026-05-03 18:56:39 +0200

    Добавлены маршруты регистрации и профиля

--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 from users.apps import UsersConfig
-from users.views import LinkTelegramView
+from users.views import LinkTelegramView, UserCreateAPIView, UserProfileView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     # Авторизация (JWT)
-    path('login/', TokenObtainPairView.as_view(), name='login'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    # Регистрация
+    path("register/", UserCreateAPIView.as_view(), name="register"),
+    # Профиль
+    path("profile/", UserProfileView.as_view(), name="profile"),
     # Telegram
-    path('telegram/link/', LinkTelegramView.as_view(), name='link-telegram'),
+    path("telegram/link/", LinkTelegramView.as_view(), name="link-telegram"),
 ]
